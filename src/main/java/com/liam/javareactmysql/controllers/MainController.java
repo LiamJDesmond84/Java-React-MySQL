7a41223b66a209d6ffc74fcd4e5ca58e922fd0d1
Liam Desmond <liamjdesmond84@gmail.com>
2022-03-31 09:54:00 -0500

    Just adding Rest Controller

--- src/main/java/com/liam/javareactmysql/controllers/MainController.java
+++ src/main/java/com/liam/javareactmysql/controllers/MainController.java
@@ -1,10 +1,12 @@
 package com.liam.javareactmysql.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.liam.javareactmysql.services.UserService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MainController {
 	
